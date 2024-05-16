eec5b013cd261d6231969a1aae257b02e2a878de
Maggie-dev93 <maggieneubiglong@gmail.com>
2024-05-16 13:30:22 +0000

    lesson 3.5 login form

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch("https://movies-flixmcn-ed96d6a64be1.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://movies-flixmcn-ed96d6a64be1.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((movie) => {
@@ -17,11 +29,11 @@ export const MainView = () => {
             image: movie.ImageUrl, // Assuming "imageurl" is "ImageUrl"
             description: movie.Description,
             director: movie.Director.Name, // Extracting director's name
-            bio: movie.Director.Bio, // Extracting director's name
-            birth: movie.Director.Birth, // Extracting director's name
-            death: movie.Director.Death, // Extracting director's name
+            bio: movie.Director.Bio, // Extracting director's bio
+            birth: movie.Director.Birth, // Extracting director's birth
+            death: movie.Director.Death, // Extracting director's death
             genre: movie.Genre.Name, // Extracting genre name
-            genre_description: movie.Genre.Description, // Extracting genre name
+            genre_description: movie.Genre.Description, // Extracting genre description
             featured: movie.featured, // Assuming "featured" is a direct property of the movie
             releaseDate: new Date(movie.ReleaseDate), // Parsing release date
           };
@@ -32,7 +44,24 @@ export const MainView = () => {
       .catch((error) => {
         console.error("Error fetching movies:", error); // Log any errors
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem("token", token);
+          }}
+        />
+        <p>or</p>
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -58,6 +87,17 @@ export const MainView = () => {
           }}
         />
       ))}
+
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.removeItem("user");
+          localStorage.removeItem("token");
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
